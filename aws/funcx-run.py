37d27b99c7a537ce251ef16284f689d3713d8217
Ryan Chard <rchard@anl.gov>
2021-11-19 11:11:38 +1300

    Check the status during a run call

--- aws/funcx-run.py
+++ aws/funcx-run.py
@@ -1,10 +1,12 @@
 import json
-from funcx.sdk.client import FuncXClient
-from globus_sdk import AccessTokenAuthorizer
-import boto3
 import uuid
+import time
+import boto3
 import datetime
 
+from funcx.sdk.client import FuncXClient
+from funcx.utils.errors import TaskPending
+from globus_sdk import AccessTokenAuthorizer
 
 def now_isoformat():
     return datetime.datetime.now().isoformat()
@@ -33,15 +35,6 @@ def lambda_handler(event, context):
     monitor_by = body['monitor_by'] if 'monitor_by' in body else None
     manage_by = body['manage_by'] if 'manage_by' in body else None
 
-    result = {
-        "action_id": action_id,
-        'status': 'ACTIVE',
-        'display_status': 'Function Submitted',
-        'details': None,
-        'monitor_by': monitor_by,
-        'manage_by': manage_by,
-        'start_time': now_isoformat(),
-    }
     tasks = {}
     batch = fxc.create_batch()
 
@@ -53,17 +46,76 @@ def lambda_handler(event, context):
     batch_res = fxc.batch_run(batch)
     print(batch_res)
 
-    # Create a dynamo record where the primary key is this action's ID
-    # Tasks is a dict by task_id and contains the eventual results from their
-    # execution. Where there are no more None results then the action is complete
-    response = table.put_item(
-        Item={
-            'action-id': action_id,
-            'tasks': json.dumps({task_id: {"result": None} for task_id in batch_res})
-        }
-    )
-    print("Dynamo", response)
+    # Give it a bit to run and then attempt to get the status
+    time.sleep(0.5)
+
+    task_results = json.dumps({task_id: {"result": None} for task_id in batch_res})
+
+    # Find the taskIDs where the results are not yet in
+    running_tasks = list(filter(lambda task_id: bool(task_id),
+                                [key if not task_results[key][
+                                    'result'] else None
+                                 for key in task_results.keys()]))
+
+    failure = None
+    if running_tasks:
+        for task in running_tasks:
+            result = None
+            try:
+                result = fxc.get_result(task)
+                print("---->", result, type(result))
+
+            except TaskPending as eek:
+                print("Faiulure ", eek)
+                result = None
+            except Exception as eek2:
+                print("Detected an exception: ", eek2)
+                failure = str(eek2)
+                result = None
+
+            if result:
+                task_results[task]['result'] = result
+
+    # If there are running tasks cache them in Tables
+    if running_tasks:
+        # Create a dynamo record where the primary key is this action's ID
+        # Tasks is a dict by task_id and contains the eventual results from their
+        # execution. Where there are no more None results then the action is complete
+        response = table.put_item(
+            Item={
+                'action-id': action_id,
+                'tasks': json.dumps({task_id: {"result": None} for task_id in batch_res})
+            }
+        )
+        print("Dynamo", response)
+        if failure:
+            status = "FAILED"
+            details = failure
+            display_status = failure
+            status_code = 200
+        else:
+            status = "ACTIVE"
+            details = None
+            status_code = 202
+    else:
+        # Return the results with a SUCCEEDED status
+        status = 'SUCCEEDED'
+        display_status = 'Function Results Received'
+        status_code = 200
+
+    result = {
+        "action_id": action_id,
+        'status': status,
+        'display_status': display_status,
+        'details': details,
+        'monitor_by': monitor_by,
+        'manage_by': manage_by,
+        'start_time': now_isoformat(),
+    }
+
+    print("Status result", result)
     return {
-        'statusCode': 202,
+        'statusCode': status_code,
         'body': json.dumps(result)
     }
+    
